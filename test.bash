ef8aa50f490d6b6571e027fa2d614eea525f89c4
lionel (pi) <lionel.desorme@free.fr>
2019-09-14 15:44:18 +0200

    .

---
+++ test.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CMD=/opt/vc/bin/vcgencmd
+
+#$CMD commands
+
+i="0"
+
+while [ $i -lt 40 ]
+do
+    $CMD measure_temp
+    $CMD measure_clock arm
+    $CMD get_throttled
+    sleep 1
+    i=$[$i+1]
+done
