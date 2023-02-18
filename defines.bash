9959bcf92013ddbbd7f01dd7308f005fc72f82ac
lionel@riscv <lionel.desorme@free.fr>
2023-02-18 16:54:47 +0100

    add test for riscv

--- defines.bash
+++ defines.bash
@@ -4,3 +4,11 @@ g++ -dM -E - < /dev/null               > ${TARGET}.txt
 g++               -Q --help=target     > ${TARGET}.default.txt
 g++ -march=native -Q --help=target     > ${TARGET}.native.txt
 
+PROC=`uname -p`
+if [ $PROC == "riscv64" ]; then
+	    g++ -march=rv64imafdcsuh -Q --help=target     > ${TARGET}.riscv.txt
+fi
+
+
+#g++ -march=rv64imafdcsuh -Q --help=target     > ${TARGET}.riscv.txt
+
