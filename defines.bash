30d9688d5dbf8f01c50709bb03bad3903ca537f3
Lionel Desorme <lionel.desorme@free.fr>
2023-02-18 17:03:13 +0100

    Redirect stderr

--- defines.bash
+++ defines.bash
@@ -1,14 +1,10 @@
 #!/bin/bash
 TARGET=_defines
-g++ -dM -E - < /dev/null               > ${TARGET}.txt
-g++               -Q --help=target     > ${TARGET}.default.txt
-g++ -march=native -Q --help=target     > ${TARGET}.native.txt
+g++ -dM -E - < /dev/null           > ${TARGET}.txt         2>&1
+g++               -Q --help=target > ${TARGET}.default.txt 2>&1
+g++ -march=native -Q --help=target > ${TARGET}.native.txt  2>&1
 
 PROC=`uname -p`
 if [ $PROC == "riscv64" ]; then
-	    g++ -march=rv64imafdcsuh -Q --help=target     > ${TARGET}.riscv.txt
+    g++ -march=rv64imafdcsuh -Q --help=target > ${TARGET}.riscv.txt 2>&1
 fi
-
-
-#g++ -march=rv64imafdcsuh -Q --help=target     > ${TARGET}.riscv.txt
-
