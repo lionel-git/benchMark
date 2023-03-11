70237ee58833c7fc3b9602af410157af7a39ac85
Lionel Desorme <lionel.desorme@free.fr>
2023-03-11 16:37:20 +0100

    Update script

--- show_freq.bash
+++ show_freq.bash
@@ -1,2 +1,10 @@
 #!/bin/bash
-sudo watch -n 1  cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_cur_freq
+CMD="cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_cur_freq"
+VCGENCMD=/usr/bin/vcgencmd
+if [ -f ${VCGENCMD} ]
+then
+   CMD="${CMD}; ${VCGENCMD} measure_temp"
+fi
+#echo $CMD
+sudo watch -n 1 ${CMD}
+
